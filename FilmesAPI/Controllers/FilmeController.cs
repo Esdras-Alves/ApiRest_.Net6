8fd293b046c90bb8c6b1b4216cc9f98a03dfde5e
esdras.alves <esdras.alves@LG003806>
2023-09-04 11:21:06 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,34 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id}, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if(filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required (ErrorMessage = "O título do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O título do filme não deve exceder 50 caracteres")]
+    public string? Titulo { get; set; }
+
+    [Required (ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(25, ErrorMessage = "O gênero do filme não deve exceder 25 caracteres")]
+    public string? Genero { get; set; }
+
+    [Required (ErrorMessage = "A duração do filme é obrigatória")]
+    [Range(70, 600, ErrorMessage ="A duração deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
